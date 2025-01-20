9484a8265c7a0a93f0b66cf0b8bb859f39711d41
Lautaro <lautarogandodev@gmail.com>
2025-01-20 18:50:32 -0300

    Scaffolding v1

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,11 +6,20 @@ export default {
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
+  darkMode: "class",
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        primaryColor: "#A79351",
+        secondaryColor: "#222222",
+        secondaryColorDark: "#E4E4E4",
+        bgColor: "#FAFAFA",
+        bgColorDark: "#000000",
+        primaryTransparentColor: "#A7935133",
+      },
+      fontFamily: {
+        montserrat: ["Montserrat", "sans-serif"],
+        playfairDisplay: ["PlayfairDisplay", "sans-serif"],
       },
     },
   },
