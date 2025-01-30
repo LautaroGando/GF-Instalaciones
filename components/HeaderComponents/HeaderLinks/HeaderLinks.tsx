dd54a55583d91423e574d6e537e71052f0c4eefb
Lautaro Gando <133940955+LautaroGando@users.noreply.github.com>
2025-01-30 01:35:33 -0300

    Update HeaderLinks.tsx

--- components/HeaderComponents/HeaderLinks/HeaderLinks.tsx
+++ components/HeaderComponents/HeaderLinks/HeaderLinks.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { IHeaderLink } from "@/interfaces/IHeaderLink";
+import { IHeaderLink } from "@/data/IHeaderLink/types.d.ts";
 import { headerLinks } from "@/data/HeaderLinks/header-links";
 import Link from "next/link";
 import React from "react";
